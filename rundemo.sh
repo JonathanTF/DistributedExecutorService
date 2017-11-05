a2d5e8d5dc9a39a9e7c96cf3045473ac76a2d9e1
Jonathan <jonny28f@gmail.com>
2017-11-05 01:17:15 -0500

    rundemo update

--- rundemo.sh
+++ rundemo.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
-echo -n "building $1"
+if [ ! -d "bin" ]
+then
+	mkdir bin
+fi
 
-javac -d bin Src/Demo/$1
+echo -n -e "building $1\n"
+
+javac -d bin/ src/distributedES/RemoteMethods.java src/distributedES/DistributedFutureTask.java src/distributedES/Node.java src/distributedES/DistributedExecutorService.java src/worknode/WorkNode.java
+
+exitValue=$?
+
+if [ $exitValue != 0 ]
+then
+echo -n -e "build failed: distrubtedES error\n"
+exit $exitValue
+fi
+
+javac -d bin/ src/Demo/SleepyHelloWorld.java src/Demo/SleepyHelloWorldCall.java src/Demo/SleepyHelloWorldCall2.java
+
+exitValue=$?
+
+if [ $exitValue != 0 ]
+then
+echo -n -e "build failed: Runnable/Callable demo error\n"
+exit $exitValue
+fi
+
+javac -d bin/ -classpath bin/ src/Demo/$1.java
+
+exitValue=$?
+
+if [ $exitValue != 0 ]
+then
+echo -n -e "failed to build $1\n"
+exit $exitValue
+fi
+
+echo -n -e "Running $1 with parameters $2:$3...\n"
+cd bin/
+java Demo.$1 $2 $3
+
